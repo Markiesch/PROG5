1cb2555f2fe2cab48c347b7c53ba3815d1c4d078
Markiesch <marksmeel@gmail.com>
2024-09-25 20:11:21 +0200

    Feat: Added Data EntityFramework classes

---
+++ NinjaManager.Data/Equipment/Equipment.cs
@@ -0,0 +1,12 @@
+﻿namespace NinjaManager.Equipment;
+
+public class Equipment
+{
+    public int Id { get; set; }
+    public int price { get; set; }
+    public string name { get; set; }
+    public int strength { get; set; }
+    public int intelligence { get; set; }
+    public int agility { get; set; }
+    public int categoryId { get; set; }
+}
---
+++ NinjaManager.Data/Ninja/Ninja.cs
@@ -0,0 +1,8 @@
+﻿namespace NinjaManager.Data;
+
+public class Ninja
+{
+    public int Id { get; set; } 
+    public string name { get; set; }
+    public int currency { get; set; }
+}
