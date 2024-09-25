70ef0afec7df7b2a533fd3cdd78b09d6c7e976a6
KorayYLM <pertev.koray@gmail.com>
2024-09-25 19:07:46 +0200

    Begin van de Ninja model en Equipment model

---
+++ Models/Equipment.cs
@@ -0,0 +1,31 @@
+﻿public class Equipment
+{
+    public int EquipmentId { get; set; }
+    public string Name { get; set; }
+    public decimal ValueInGold { get; set; }
+    public int Strength { get; set; }
+    public int Intelligence { get; set; }
+    public int Agility { get; set; }
+
+    // Equipment category: Head, Chest, Hand, Feet, Ring, Necklace
+    public EquipmentCategory Category { get; set; }
+
+    // Relationship with Ninja (Many-to-Many)
+    public virtual ICollection<Ninja> Ninjas { get; set; }
+
+    public Equipment()
+    {
+        Ninjas = new List<Ninja>();
+    }
+}
+
+public enum EquipmentCategory
+{
+    Head,
+    Chest,
+    Hand,
+    Feet,
+    Ring,
+    Necklace
+}
+
---
+++ Models/Ninja.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+public class Ninja
+{
+    public int NinjaId { get; set; }
+    public string Name { get; set; }
+    public decimal Gold { get; set; }
+    
+    // Inventory: List of Equipment
+    public virtual ICollection<Equipment> Inventory { get; set; }
+
+    // Calculated stats
+    public int TotalStrength => Inventory?.Sum(e => e.Strength) ?? 0;
+    public int TotalIntelligence => Inventory?.Sum(e => e.Intelligence) ?? 0;
+    public int TotalAgility => Inventory?.Sum(e => e.Agility) ?? 0;
+    public decimal TotalGoldValue => Inventory?.Sum(e => e.ValueInGold) ?? 0;
+
+    public Ninja()
+    {
+        Inventory = new List<Equipment>();
+    }
+}
