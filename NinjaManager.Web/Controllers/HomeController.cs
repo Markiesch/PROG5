6ec295bddf1b652180188e466fe0ecbe9c25ce66
Markiesch <marksmeel@gmail.com>
2024-09-26 15:09:00 +0200

    Fix: styling

--- NinjaManager.Web/Controllers/HomeController.cs
+++ NinjaManager.Web/Controllers/HomeController.cs
@@ -18,16 +18,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    public IActionResult SelectNinja()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
---
+++ NinjaManager.Web/Controllers/NinjaController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NinjaManager.Controllers;
+
+public class NinjaController : Controller
+{
+    private readonly ILogger<NinjaController> _logger;
+
+    public NinjaController(ILogger<NinjaController> logger)
+    {
+        _logger = logger;
+    }
+    
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
