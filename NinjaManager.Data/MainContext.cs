44f65164cdee9de8e93348450350ac67613b6ce6
Markiesch <marksmeel@gmail.com>
2024-09-26 16:23:50 +0200

    Realistische data in seeder gezet

--- NinjaManager.Data/MainContext.cs
+++ NinjaManager.Data/MainContext.cs
@@ -32,46 +32,46 @@ public class MainContext : DbContext
         
         modelBuilder.Entity<Equipment>().HasData(
             // Head
-            new Equipment { Id = 1, CategoryId = 1, Img = "helmet_01a",  Name = "Leather Helmet", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 2, CategoryId = 1, Img = "helmet_01b",  Name = "Iron Helmet", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 3, CategoryId = 1, Img = "helmet_01c",  Name = "Golden Helmet", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 4, CategoryId = 1, Img = "helmet_01d",  Name = "Diamond Helmet", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 5, CategoryId = 1, Img = "helmet_01e",  Name = "Mythic Helmet", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
+            new Equipment { Id = 1, CategoryId = 1, Img = "helmet_01a",  Name = "Leather Helmet", Price = 300, Strength = 2, Intelligence = 0, Agility = 1 },
+            new Equipment { Id = 2, CategoryId = 1, Img = "helmet_01b",  Name = "Iron Helmet", Price = 450, Strength = 5, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 3, CategoryId = 1, Img = "helmet_01c",  Name = "Golden Helmet", Price = 700, Strength = 7, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 4, CategoryId = 1, Img = "helmet_01d",  Name = "Diamond Helmet", Price = 1000, Strength = 10, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 5, CategoryId = 1, Img = "helmet_01e",  Name = "Mythic Helmet", Price = 1500, Strength = 15, Intelligence = 2, Agility = 4 },
 
             // Necklaces
-            new Equipment { Id = 6, CategoryId = 2, Img = "necklace_03a",  Name = "Leather Necklace", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 7, CategoryId = 2, Img = "necklace_03b",  Name = "Iron Necklace", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 8, CategoryId = 2, Img = "necklace_03c",  Name = "Golden Necklace", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 9, CategoryId = 2, Img = "necklace_03d",  Name = "Diamond Necklace", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 10, CategoryId = 2, Img = "necklace_03e",  Name = "Mythic Necklace", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
+            new Equipment { Id = 6, CategoryId = 2, Img = "necklace_03a",  Name = "Leather Necklace", Price = 200, Strength = 1, Intelligence = 1, Agility = 0 },
+            new Equipment { Id = 7, CategoryId = 2, Img = "necklace_03b",  Name = "Iron Necklace", Price = 400, Strength = 3, Intelligence = 2, Agility = 1 },
+            new Equipment { Id = 8, CategoryId = 2, Img = "necklace_03c",  Name = "Golden Necklace", Price = 600, Strength = 5, Intelligence = 3, Agility = 1 },
+            new Equipment { Id = 9, CategoryId = 2, Img = "necklace_03d",  Name = "Diamond Necklace", Price = 900, Strength = 7, Intelligence = 4, Agility = 2 },
+            new Equipment { Id = 10, CategoryId = 2, Img = "necklace_03e",  Name = "Mythic Necklace", Price = 1400, Strength = 10, Intelligence = 5, Agility = 3 },
             
             // Chest
-            new Equipment { Id = 11, CategoryId = 3, Img = "armor_01a",  Name = "Leather Chestplate", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 12, CategoryId = 3, Img = "armor_01b",  Name = "Iron Chestplate", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 13, CategoryId = 3, Img = "armor_01c",  Name = "Golden Chestplate", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 14, CategoryId = 3, Img = "armor_01d",  Name = "Diamond Chestplate", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 15, CategoryId = 3, Img = "armor_01e",  Name = "Mythic Chestplate", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
+            new Equipment { Id = 11, CategoryId = 3, Img = "armor_01a",  Name = "Leather Chestplate", Price = 350, Strength = 3, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 12, CategoryId = 3, Img = "armor_01b",  Name = "Iron Chestplate", Price = 500, Strength = 6, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 13, CategoryId = 3, Img = "armor_01c",  Name = "Golden Chestplate", Price = 750, Strength = 9, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 14, CategoryId = 3, Img = "armor_01d",  Name = "Diamond Chestplate", Price = 1200, Strength = 12, Intelligence = 1, Agility = 4 },
+            new Equipment { Id = 15, CategoryId = 3, Img = "armor_01e",  Name = "Mythic Chestplate", Price = 1800, Strength = 18, Intelligence = 3, Agility = 5 },
 
             // Hands
-            new Equipment { Id = 16, CategoryId = 4, Img = "gloves_01a",  Name = "Leather Gloves", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 17, CategoryId = 4, Img = "gloves_01b",  Name = "Iron Gloves", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 18, CategoryId = 4, Img = "gloves_01c",  Name = "Golden Gloves", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 19, CategoryId = 4, Img = "gloves_01d",  Name = "Diamond Gloves", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 20, CategoryId = 4, Img = "gloves_01e",  Name = "Mythic Gloves", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
+            new Equipment { Id = 16, CategoryId = 4, Img = "gloves_01a",  Name = "Leather Gloves", Price = 200, Strength = 1, Intelligence = 0, Agility = 1 },
+            new Equipment { Id = 17, CategoryId = 4, Img = "gloves_01b",  Name = "Iron Gloves", Price = 400, Strength = 3, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 18, CategoryId = 4, Img = "gloves_01c",  Name = "Golden Gloves", Price = 600, Strength = 5, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 19, CategoryId = 4, Img = "gloves_01d",  Name = "Diamond Gloves", Price = 800, Strength = 7, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 20, CategoryId = 4, Img = "gloves_01e",  Name = "Mythic Gloves", Price = 1200, Strength = 10, Intelligence = 1, Agility = 4 },
                 
             // Rings
-            new Equipment { Id = 21, CategoryId = 5, Img = "ring_01a",  Name = "Leather Ring", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 22, CategoryId = 5, Img = "ring_01b",  Name = "Iron Ring", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 23, CategoryId = 5, Img = "ring_01c",  Name = "Golden Ring", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 24, CategoryId = 5, Img = "ring_01d",  Name = "Diamond Ring", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 25, CategoryId = 5, Img = "ring_01e",  Name = "Mythic Ring", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
+            new Equipment { Id = 21, CategoryId = 5, Img = "ring_01a",  Name = "Leather Ring", Price = 150, Strength = 1, Intelligence = 1, Agility = 0 },
+            new Equipment { Id = 22, CategoryId = 5, Img = "ring_01b",  Name = "Iron Ring", Price = 300, Strength = 2, Intelligence = 2, Agility = 1 },
+            new Equipment { Id = 23, CategoryId = 5, Img = "ring_01c",  Name = "Golden Ring", Price = 500, Strength = 4, Intelligence = 3, Agility = 1 },
+            new Equipment { Id = 24, CategoryId = 5, Img = "ring_01d",  Name = "Diamond Ring", Price = 800, Strength = 6, Intelligence = 4, Agility = 2 },
+            new Equipment { Id = 25, CategoryId = 5, Img = "ring_01e",  Name = "Mythic Ring", Price = 1200, Strength = 8, Intelligence = 6, Agility = 3 },
                 
             // Boots
-            new Equipment { Id = 26, CategoryId = 6, Img = "boots_01a",  Name = "Leather Boots", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 27, CategoryId = 6, Img = "boots_01b",  Name = "Iron Boots", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 28, CategoryId = 6, Img = "boots_01c",  Name = "Golden Boots", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 29, CategoryId = 6, Img = "boots_01d",  Name = "Diamond Boots", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 },
-            new Equipment { Id = 30, CategoryId = 6, Img = "boots_01e",  Name = "Mythic Boots", Price = 300, Strength = 0, Intelligence = 0, Agility = 0 }
+            new Equipment { Id = 26, CategoryId = 6, Img = "boots_01a",  Name = "Leather Boots", Price = 250, Strength = 1, Intelligence = 0, Agility = 2 },
+            new Equipment { Id = 27, CategoryId = 6, Img = "boots_01b",  Name = "Iron Boots", Price = 450, Strength = 3, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 28, CategoryId = 6, Img = "boots_01c",  Name = "Golden Boots", Price = 650, Strength = 5, Intelligence = 0, Agility = 3 },
+            new Equipment { Id = 29, CategoryId = 6, Img = "boots_01d",  Name = "Diamond Boots", Price = 900, Strength = 7, Intelligence = 1, Agility = 4 },
+            new Equipment { Id = 30, CategoryId = 6, Img = "boots_01e",  Name = "Mythic Boots", Price = 1400, Strength = 10, Intelligence = 2, Agility = 5 }       
         );
     }
 }
---
+++ NinjaManager.Data/Migrations/20240926142329_EquipmentConfig.cs
@@ -0,0 +1,438 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NinjaManager.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class EquipmentConfig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Agility", "Strength" },
+                values: new object[] { 1, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 450, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 700, 7 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 1000, 10 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 4, 2, 1500, 15 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Intelligence", "Price", "Strength" },
+                values: new object[] { 1, 200, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 1, 2, 400, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 1, 3, 600, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 2, 4, 900, 7 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 3, 5, 1400, 10 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 350, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 500, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 750, 9 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 4, 1, 1200, 12 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 5, 3, 1800, 18 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 1, 200, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 400, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 600, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 800, 7 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 4, 1, 1200, 10 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "Intelligence", "Price", "Strength" },
+                values: new object[] { 1, 150, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 22,
+                columns: new[] { "Agility", "Intelligence", "Strength" },
+                values: new object[] { 1, 2, 2 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 23,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 1, 3, 500, 4 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 24,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 2, 4, 800, 6 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 25,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 3, 6, 1200, 8 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 26,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 2, 250, 1 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 27,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 450, 3 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 28,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 3, 650, 5 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 29,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 4, 1, 900, 7 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 30,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 5, 2, 1400, 10 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Agility", "Strength" },
+                values: new object[] { 0, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 3,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 5,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 6,
+                columns: new[] { "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 7,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 8,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 9,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 10,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 11,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 12,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 13,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 14,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 15,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 16,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 17,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 18,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 19,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 20,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 21,
+                columns: new[] { "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 22,
+                columns: new[] { "Agility", "Intelligence", "Strength" },
+                values: new object[] { 0, 0, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 23,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 24,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 25,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 26,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 27,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 28,
+                columns: new[] { "Agility", "Price", "Strength" },
+                values: new object[] { 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 29,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+
+            migrationBuilder.UpdateData(
+                table: "Equipments",
+                keyColumn: "Id",
+                keyValue: 30,
+                columns: new[] { "Agility", "Intelligence", "Price", "Strength" },
+                values: new object[] { 0, 0, 300, 0 });
+        }
+    }
+}
