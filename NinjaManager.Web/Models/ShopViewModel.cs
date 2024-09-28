201dd5201c3c24afa8ecbb086f58165f6626ed16
Markiesch <marksmeel@gmail.com>
2024-09-28 23:31:36 +0200

    Buy functionaliteit toegevoegd

---
+++ NinjaManager.Web/Models/ShopViewModel.cs
@@ -0,0 +1,15 @@
+﻿using NinjaManager.Data;
+
+namespace NinjaManager.Web.Models;
+
+public class ShopViewModel
+{
+    public ShopViewModel(int id, List<Equipment> items)
+    {
+        Id = id;
+        Items = items;
+    }
+    
+    public int Id { get; set; }
+    public List<Equipment> Items { get; set; }
+}
