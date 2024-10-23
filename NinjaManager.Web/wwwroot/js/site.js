e43d066c14882bfd71c7dbfdacffeccfeeebe21d
Markiesch <marksmeel@gmail.com>
2024-10-23 21:51:58 +0200

    Homepage verbeterd

--- NinjaManager.Web/wwwroot/js/site.js
+++ NinjaManager.Web/wwwroot/js/site.js
@@ -26,3 +26,31 @@ inputs.forEach(input => {
         input.value = input.value.toUpperCase();
     });
 });
+
+const scrollContainer = document.querySelector('.scroll-container');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener('mousedown', (e) => {
+    isDown = true;
+    startX = e.pageX - scrollContainer.offsetLeft;
+    scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+    isDown = false;
+});
+
+scrollContainer.addEventListener('mouseup', () => {
+    isDown = false;
+});
+
+scrollContainer.addEventListener('mousemove', (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - scrollContainer.offsetLeft;
+    const walk = (x - startX) ; // Scroll speed
+    scrollContainer.scrollLeft = scrollLeft - walk;
+});
